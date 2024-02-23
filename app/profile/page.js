91ced2c38341834c73b99679f3af85e134891daa
Dimitar Georgiev <deeperffect@gmail.com>
2024-02-23 16:14:33 +0200

    deployment build7

--- app/profile/page.js
+++ app/profile/page.js
@@ -8,28 +8,26 @@ import PostedBlogs from "@components/user/PostedBlogs";
 const Profile = () => {
 	return (
 		<>
-			<Section>
-				<div className="flex flex-col">
-					<div className="bg-white/50 py-4">
-						<Section>
-							<h2 className="text-3xl py-4 font-bold">Listed items</h2>
-							<ListedItems />
-						</Section>
-					</div>
-					<div className="bg-slate-200/50 py-4">
-						<Section>
-							<h2 className="text-3xl py-4 font-bold">Favorite items</h2>
-							<FavItems />
-						</Section>
-					</div>
-					<div className="bg-white/50 py-4">
-						<Section>
-							<h2 className="text-3xl py-4 font-bold">Blogs posted</h2>
-							<PostedBlogs />
-						</Section>
-					</div>
+			<div className="flex flex-col">
+				<div className="bg-white/50 p-4">
+					<Section>
+						<h2 className="text-3xl py-4 font-bold">Listed items</h2>
+						<ListedItems />
+					</Section>
 				</div>
-			</Section>
+				<div className="bg-slate-200/50 p-4">
+					<Section>
+						<h2 className="text-3xl py-4 font-bold">Favorite items</h2>
+						<FavItems />
+					</Section>
+				</div>
+				<div className="bg-white/50 p-4">
+					<Section>
+						<h2 className="text-3xl py-4 font-bold">Blogs posted</h2>
+						<PostedBlogs />
+					</Section>
+				</div>
+			</div>
 		</>
 	)
 };
